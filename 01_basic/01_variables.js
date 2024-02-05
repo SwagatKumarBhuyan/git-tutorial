e66748e501316b8f0496d02b22c459aa97c2ac7c
SwagatKumarBhuyan <124615710+SwagatKumarBhuyan@users.noreply.github.com>
2024-02-05 02:45:33 +0000

    new

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=144553
+let accountEmail="swagat@gmail.com"
+var accountPassword="12345"
+accountCity="Puri"
+
+// accountId=2//not allowed
+
+accountEmail="swa@.com"
+accountPassword="2121221"
+accountCity="BBSR"
+
+console.log(accountId);
+
+console.table([accountId,accountEmail,accountPassword,accountCity]);
